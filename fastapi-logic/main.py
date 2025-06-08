3dd04f33ae75e05c6d4e88be55ced18d0deabe83
Tusiime Mark <tusiimejohnmark@gmail.com>
2025-06-08 00:57:18 -0700

    feat: calculate_loan API & logic

--- fastapi-logic/main.py
+++ fastapi-logic/main.py
@@ -52,7 +52,36 @@ def calculate_advance(req: AdvanceRequest):
 
     if not eligible or req.advance_amount > max_advance:
         return {"eligible": False, "max_advance": round(max_advance, 2)}
+    
+    df = pd.DataFrame({
+        "advance_amount": [req.advance_amount],
+        "fee_rate": [FEE_RATE]
+    })
+    df["fee"] = df["advance_amount"] * df["fee_rate"]
+    df["total_repayable"] = df["advance_amount"] + df ["fee"]
 
+    return {
+        "eligible": True,
+        "fee_rate": FEE_RATE,
+        "fee": round(df.loc[0, "fee"], 2),
+        "total_repayable": round(df.loc[0, "total_repayable"], 2),
+        "max_advance": round(max_advance, 2)
+    }
+
+
+
+@app.post("/calculate_loan")
+def calculate_loan(req: LoanRequest):
+    df, emi = generate_amortisation_schedule(req.loan_amount, req.interest_rate, req.loan_term)
+    total_repayable = df['EMI'].sum()
+
+    schedule = df.to_dict(orient = "records")
+
+    return {
+        "emi": round (emi, 2),
+        "total_repayable": round(total_repayable, 2),
+        "schedule": schedule
+    }
 
 
 
